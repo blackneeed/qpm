269877943698f5514499c37a4c99938faa414b96
Fivvee0 <167813307+Fivvee0@users.noreply.github.com>
2025-04-25 22:16:03 +0200

    Implement command-line argument parsing using Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "qpm")]
+#[command(about = "qpm - A simple package manager for Linux (and Windows)")]
+#[command(version = "0.1.0")]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Install a package")]
+    Install {
+        #[arg(help = "Name of the package to install")]
+        package: String,
+    },
+
+    #[command(about = "Show qpm information")]
+    Info { package: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+
+    match &cli.command {
+        Commands::Install { package } => {}
+        Commands::Info { package } => {}
+    }
 }
